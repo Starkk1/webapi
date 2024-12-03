9bd4cb02dec2d16e4b92d1cc28726fad3820d7f9
Adel <therealstark13@gmail.com>
2024-12-03 22:08:53 +0200

    I've been commiting for way too long

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -11,10 +11,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // Commented out to disable HTTPS redirection
+app.UseAuthorization();
+app.MapControllers();
 
 var summaries = new[]
 {
